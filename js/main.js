f32df34c950411daeab43b96fdb3f1343b9b67ce
moimenwagdy <moimenwy@gmail.com>
2023-06-18 18:13:23 +0300

    now you can watch the videos and also open the images in a hight resolution with some design corrections

--- js/main.js
+++ js/main.js
@@ -3,9 +3,19 @@ let getImg = (link) => {
   img.title = `${link}`;
   img.src = link;
   img.classList.add("img-fluid");
-  // img.width = width;
-  // img.height = height;
   return img;
+};
+
+let getVideo = (link) => {
+  let newLink = link.replace("watch?v=", "embed/");
+  let regEx =
+    /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  let vidId = newLink.match(regEx)[1];
+  let vid = document.querySelector(".vid-loc");
+  vid.src = `https://www.youtube.com/embed/${vidId}`;
+  vid.title = "YouTube video player";
+
+  return vid;
 };
 
 async function getData() {
@@ -20,22 +30,35 @@ async function getData() {
   } finally {
   }
 }
-let response = getData();
 
-response
-  .then((finalLength) => {
-    return finalLength;
-  })
-  .then((use) => {
-    if (use.media_type === "image") {
-      let resized = getImg(use.url);
-      return resized;
-    }
-  })
-  .then((returned) => {
-    let position = document.querySelector(".imageContainer");
-    position.append(returned);
+let response = getData();
+let position = document.querySelector(".imageContainer");
+let hideToogle = document.querySelector(".hideToggle");
+let imgcont = document.querySelector(".removableImgCont");
+let vidcont = document.querySelector(".vidContainer");
+let myImg = document.querySelector(".img-big");
+
+response.then((use) => {
+  if (use.media_type === "image") {
+    vidcont.style.display = "none";
+    let myImage = getImg(use.url);
+    position.append(myImage);
+  }
+  if (use.media_type === "video") {
+    imgcont.style.cssText = "display:none !important; ";
+    return getVideo(use.url);
+  }
+
+  position.addEventListener("click", () => {
+    hideToogle.classList.toggle("d-flex");
+    hideToogle.classList.toggle("d-none");
+    myImg.src = use.url;
   });
+});
+hideToogle.addEventListener("click", () => {
+  hideToogle.classList.toggle("d-flex");
+  hideToogle.classList.toggle("d-none");
+});
 
 response.then((data) => {
   let explain = document.querySelector(".explanation");
